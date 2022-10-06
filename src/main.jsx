5f26cbe7bea05182d4e2c095d2851283f9dd68dc
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-10-06 16:56:09 +0300

    outler router added

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,21 @@ import {
 } from "react-router-dom";
 import Root from './routes/root';
 import ErrorPage from './error-page';
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
     errorElement: <ErrorPage></ErrorPage>,
+    children:[
+      {
+        path: "contacts/:contactId",
+        element: <Contact></Contact>,
+      },
+    ],
   },
+  
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Outlet, Link } from "react-router-dom";
 
 const Root = () => {
     return (
@@ -31,15 +32,17 @@ const Root = () => {
                 <nav>
                     <ul>
                         <li>
-                            <a href={`contacts/1`}>Your Name</a>
+                            <Link to={`contacts/1`}>Your Name</Link>
                         </li>
                         <li>
-                            <a href={`contacts/2`}>Your Friend</a>
+                            <Link to={`contacts/2`}>Your Friend</Link>
                         </li>
                     </ul>
                 </nav>
             </div>
-            <div id="detail"></div>
+            <div id="detail">
+                <Outlet></Outlet>
+            </div>
         </>
     );
 };
